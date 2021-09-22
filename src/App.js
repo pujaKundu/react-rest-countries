ba6054e6888c5501bc91a66f85282cb584b97710
Puja Kundu <pujakundu797@gmail.com>
2021-09-22 18:23:47 +0600

    components added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Countries></Countries>
     </div>
   );
 }
 
-function Countries(){
-  const [countries,setCountries] = useState([]);
-  useEffect(()=>{
-    fetch('https://raw.githubusercontent.com/ProgrammingHero1/rest-countries-data/main/coutries.JSON?fbclid=IwAR319ZwBtmXSFpyopiOLRg_yiQPgTJLGE-hDnttaQvVC2ZNG1yi6cp15mRU')
-    .then(res => res.json())
-    .then(data => setCountries(data))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Countries(){
+//   const [countries,setCountries] = useState([]);
+//   useEffect(()=>{
+//     fetch('https://raw.githubusercontent.com/ProgrammingHero1/rest-countries-data/main/coutries.JSON?fbclid=IwAR319ZwBtmXSFpyopiOLRg_yiQPgTJLGE-hDnttaQvVC2ZNG1yi6cp15mRU')
+//     .then(res => res.json())
+//     .then(data => setCountries(data))
     
-  },[])
-  return (
-    <div>
-      <h2>Travelling around the world</h2>
-      <h4>Countries Available : {countries.length}</h4>
-    </div>
-  )
-}
+//   },[])
+//   return (
+//     <div>
+//       <h2>Travelling around the world</h2>
+//       <h4>Countries Available : {countries.length}</h4>
+//       {
+//         countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
+//       }
+//     </div>
+//   )
+// }
+
+// function Country(props){
+//   return (
+//     <div>
+//       <h2>{props.name}</h2>
+//       <p>{props.capital}</p>
+//     </div>
+//   )
+// }
 
 export default App;
